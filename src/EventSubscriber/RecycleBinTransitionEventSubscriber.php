3a952ddaf541d967475e3e48d296ab0f494e38a6
Andrii Mutylo <amutylo@webcodin.com>
2020-01-14 18:22:05 +0200

    update node actions, event subscriber

---
+++ src/EventSubscriber/RecycleBinTransitionEventSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\recycle_bin\EventSubscriber;
+
+use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\recycle_bin\WorkflowHelper;
+use Drupal\state_machine\Event\WorkflowTransitionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\state_machine\Plugin\Workflow\WorkflowInterface;
+use Drupal\state_machine\Plugin\Workflow\WorkflowState;
+
+class RecycleBinTransitionEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The workflow helper.
+   *
+   * @var \Drupal\recycle_bin\WorkflowHelper
+   */
+  protected $workflowHelper;
+
+  public function __construct(WorkflowHelper $workflowHelper) {
+    $this->workflowHelper = $workflowHelper;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function getSubscribedEvents() {
+    return [
+      'state_machine.pre_transition' => 'handleAction',
+    ];
+  }
+
+  public function handleAction(WorkflowTransitionEvent $event) {
+    $entity = $event->getEntity();
+
+    // Verify if the new state is marked as published state.
+    $is_published_state = $this->isPublishedState($event->getToState(), $event->getWorkflow());
+
+    if ($entity instanceof EntityPublishedInterface) {
+      if ($is_published_state) {
+        $entity->setPublished();
+      }
+      else {
+        $entity->setUnpublished();
+      }
+    }
+  }
+
+  /**
+   * Checks if a state is set as published in a certain workflow.
+   *
+   * @param \Drupal\state_machine\Plugin\Workflow\WorkflowState $state
+   *   The state to check.
+   * @param \Drupal\state_machine\Plugin\Workflow\WorkflowInterface $workflow
+   *   The workflow the state belongs to.
+   *
+   * @return bool
+   *   TRUE if the state is set as published in the workflow, FALSE otherwise.
+   */
+  protected function isPublishedState(WorkflowState $state, WorkflowInterface $workflow) {
+    return $this->workflowHelper->isWorkflowStatePublished($state->getId(), $workflow);
+  }
+}
---
+++ src/WorkflowHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\recycle_bin;
+
+use Drupal\Component\Plugin\PluginInspectionInterface;
+use Drupal\state_machine\Plugin\Workflow\WorkflowInterface;
+
+class WorkflowHelper {
+  /**
+   * Checks if a state is set as published in a certain workflow.
+   *
+   * @param string $state_id
+   *   The ID of the state to check.
+   * @param \Drupal\state_machine\Plugin\Workflow\WorkflowInterface $workflow
+   *   The workflow the state belongs to.
+   *
+   * @return bool|null
+   *   TRUE|FALSE if the state is set in the workflow, NULL otherwise.
+   *
+   * @throwns \InvalidArgumentException
+   *   Thrown when the workflow is not plugin based, because this is required to
+   *   retrieve the publication state from the workflow states.
+   */
+  public function isWorkflowStatePublished($state_id, WorkflowInterface $workflow) {
+    // We rely on being able to inspect the plugin definition. Throw an error if
+    // this is not the case.
+    if (!$workflow instanceof PluginInspectionInterface) {
+      $label = $workflow->getLabel();
+      throw new \InvalidArgumentException("The '$label' workflow is not plugin based.");
+    }
+    // Retrieve the raw plugin definition, as all additional plugin settings
+    // are stored there.
+    $raw_workflow_definition = $workflow->getPluginDefinition();
+    return $raw_workflow_definition['states'][$state_id]['published'] ?? NULL;
+  }
+}
